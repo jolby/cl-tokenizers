8f245e23358a8e043176448bf5f1f9dacff9a973
Joel Boehland <jboehland@gmail.com>
2024-07-30 12:00:32 -0700

    Rename generic param to indicate ...
    ... that this can take other than text input.

--- src/protocols.lisp
+++ src/protocols.lisp
@@ -6,8 +6,8 @@
 (defgeneric get-encoder-for-model (provider-designator model-designator)
   (:documentation "Get an encoder for the given provider and model designators"))
 
-(defgeneric encode (encoder text)
-  (:documentation "Encode a piece of text using the given encoder"))
+(defgeneric encode (encoder thing-to-encode)
+  (:documentation "Encode something (string, file, bytes etc.) using the given encoder"))
 
 (defgeneric decode (encoder tokens)
-  (:documentation "Decode a list of tokens using the given encoder"))
+  (:documentation "Decode a sequence of tokens using the given encoder"))
